e056a11b88ced88c3e8f184c753ed79932a68e8b
sarah-saas <astra.vicki@outlook.com>
2024-05-03 03:34:23 -0300

    Update pesquisaGif.java

--- trabalhoAv1/src/main/java/pesquisaGif.java
+++ trabalhoAv1/src/main/java/pesquisaGif.java
@@ -1,10 +1,10 @@
-
-
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
+//import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
 /**
@@ -25,16 +25,17 @@ public class pesquisaGif extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String[] opcao = request.getParameterValues("pinguim");
+		HttpSession session = request.getSession();		
+		session.setAttribute("penguim", opcao);
+		response.sendRedirect("giphy.jsp");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
 	}
 
 }
